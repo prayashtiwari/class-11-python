cc4f08219a96214227defaefdc3409a210c2299f
prayashtiwari <tiwariprayash1200@gmail.com>
2026-01-16 21:00:25 +0530

    add basic python programs

---
+++ basics/arithmetic-operations.py
@@ -0,0 +1,9 @@
+#python program to input two numbers and perform basic arithmetic operations
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Printing the results of arithmetic operations:")
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
+print("Modulus: ", num1 % num2)

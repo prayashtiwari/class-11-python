7dbb73db2dac912bf2e0297245fed74caf6774fc
prayashtiwari <tiwariprayash1200@gmail.com>
2026-01-16 19:03:21 +0530

    rename py1.py to swap-variable.py

--- py1.py
+++
@@ -1,9 +0,0 @@
-#Program to swap two numbers using a third variable.
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-print("Values of variables before swapping: x =", x, "y =", y)
-temp = x
-x = y
-y = temp
-print("Values of variables after swapping: x =", x, "y =", y)
-#end of program

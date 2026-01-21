df28e50ad759bccd1ad32e0b2d0230c27a345e8e
prayashtiwari <tiwariprayash1200@gmail.com>
2026-01-21 18:35:28 +0530

    basic

--- basics/multiplicativetable.py
+++
@@ -1,8 +0,0 @@
-#program to input the multiplier and the multiplicand and display the multiplication table.
-num1 = int(input("Enter the multiplier: "))
-num2 = int(input("Enter the multiplicand: "))
-while num2 <= 10:
-    result = num1 * num2
-    print(num1, "x", num2, "=", result)
-    num2 += 1
-# End of the program
--- pattern.py
+++
@@ -1,6 +0,0 @@
-#python program to follow a pattern.
-rows = int(input("Please enter the number of rows: "))
-for x in range(rows, 0, -1):
-    for y in range(1, x+1):
-        print(y, end=" ")
-    print()

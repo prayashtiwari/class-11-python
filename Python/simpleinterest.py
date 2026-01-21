d5ee5c10461a44889a9a2d67d5e2fb5e0a4efa11
prayashtiwari <tiwariprayash1200@gmail.com>
2026-01-21 18:33:49 +0530

    delete

--- Python/simpleinterest.py
+++
@@ -1,6 +0,0 @@
-#reading principal amount, rate and time.
-principal = float(input("enter amount:"))
-time = float(input("enter time in years: "))
-rate = float(input("enter rate of interest: "))
-simpleinterest = (principal * rate * time) / 100
-print("The simple interest is:", simpleinterest)
